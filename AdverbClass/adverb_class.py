52ca755295e52e0e7e8d1f4fc5e1bc66b062b45f
IsachenkoBogdan <isachenko.bogdan@gmail.com>
2022-09-26 20:21:38 +0700

    AAA HW2 TASK: inheritance fixed.

--- AdverbClass/adverb_class.py
+++ AdverbClass/adverb_class.py
@@ -2,28 +2,34 @@ from keyword import iskeyword
 
 
 class ColorizeMixin:
-    def __str__(self):
+    def __str__(self) -> str:
         return f"\033[1;{self.repr_color_code}m{self.__repr__()}"
 
 
-class Advert(ColorizeMixin):
-    repr_color_code = 33
-
+class NestedAttribute:
     def __init__(self, dictionary: dict):
         self.__dict__ = \
-            {k + '_' * iskeyword(k): Advert(v) if isinstance(v, dict) else v
-             for k, v in dictionary.items()}
-        self.price = self.__dict__.get('price', 0)
+            {k + '_' * iskeyword(k): NestedAttribute(v) if isinstance(v, dict)
+             else v for k, v in dictionary.items()}
+
+
+class Advert(ColorizeMixin, NestedAttribute):
+    repr_color_code: int = 33
+
+    def __init__(self, dictionary: dict):
+        super().__init__(dictionary)
+        self.price: int = self.__dict__.get('price', 0)
+        print(self.__dict__)
 
     @property
-    def price(self):
+    def price(self) -> int:
         return self._price
 
     @price.setter
-    def price(self, value):
+    def price(self, value: int):
         if value < 0:
             raise ValueError("Price must be >= 0")
         self._price = value
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'{self.title} | {self.price} ₽'
